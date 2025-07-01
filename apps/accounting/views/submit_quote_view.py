73276ed7040d568b1d48e0d153f02044865ddd6a
Corrin Lakeland <corrin.lakeland@cmeconnect.com>
2025-07-01 15:13:19 +1200

    old cfield names

--- apps/accounting/views/submit_quote_view.py
+++ apps/accounting/views/submit_quote_view.py
@@ -130,7 +130,7 @@ def create_pdf(job):
     pdf.drawString(
         120,
         700,
-        f"{job.client.email if job.client.email else job.contact_person or 'N/A'}",
+        f"{job.client.email if job.client.email else (job.contact.email if job.contact else 'N/A')}",
     )
 
     pdf.setFont("Helvetica-Bold", 12)
